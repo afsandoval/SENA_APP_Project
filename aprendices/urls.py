67bcbfba6b5e78d6f586aa6c96fde17ec3c1c240
Felipe Sandoval <afsandovalhiguera@gmail.com>
2025-11-28 08:26:21 -0500

    Upload the Project to the new repository

---
+++ aprendices/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views 
+
+app_name = 'aprendices'
+
+urlpatterns = [
+    path('', views.inicio, name='inicio'),
+    path('aprendices/', views.aprendices, name='lista_aprendices'),
+    path('aprendices/aprendiz/<int:id_aprendiz>/', views.detalle_aprendiz, name='detalle_aprendiz'),
+    ]
