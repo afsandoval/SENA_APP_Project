24ffd176e547ec9c6cd6181768152b8552c4602b
Felipe Sandoval <afsandovalhiguera@gmail.com>
2025-12-05 10:03:57 -0500

    Update Aprendiz

--- aprendices/urls.py
+++ aprendices/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('aprendices/', views.aprendices, name='lista_aprendices'),
     path('aprendices/aprendiz/<int:id_aprendiz>/', views.detalle_aprendiz, name='detalle_aprendiz'),
     path('aprendices/crear/', views.AprendizCreateView.as_view(), name='crear_aprendiz'),
+    path('aprendices/<int:aprendiz_id>/editar/', views.AprendizUpdateView.as_view(), name='editar_aprendiz'),
     ]
